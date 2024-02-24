17b4e7785b7a087689410c96b29ba812450218e2
qdar <59022994+qdar@users.noreply.github.com>
2024-02-24 17:59:59 +0300

    header

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,30 @@
-let menu    = document.querySelector('.burger');
 let burger  = document.querySelectorAll('.js-burger');
+let menu    = document.querySelector('.burger-panel');
+let burgerButton  = document.querySelector('.burger');
+let body  = document.querySelector('body');
+
 let i;
 for (i = 0; i < burger.length; i++) {
   burger[i].onclick = function() {
-    menu.classList.toggle("burger--open");
+    menu.classList.toggle("active");
+    burgerButton.classList.toggle("burger--open");
+    body.classList.toggle("body-lock");
   }
 };
+
+window.onscroll = function() {myFunction()};
+let headerGo = document.querySelector('.header');
+let main = document.querySelector('main');
+let sticky = headerGo.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    headerGo.classList.add("header--sticky");
+    main.classList.add("main");
+  } else {
+    headerGo.classList.remove("header--sticky");
+    main.classList.remove("main");
+  }
+}
 
 let acc = document.getElementsByClassName("tab__button");
 for (i = 0; i < acc.length; i++) {
