80051dc08f1f78c366309f35fb351c6c71845429
qdar <59022994+qdar@users.noreply.github.com>
2024-03-09 00:48:03 +0300

    review and animation

--- source/js/script.js
+++ source/js/script.js
@@ -39,7 +39,6 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
-
 const swiper = new Swiper('.review__swiper', {
   slidesPerView: 1,
   spaceBetween: 16,
@@ -54,3 +53,38 @@ const swiper = new Swiper('.review__swiper', {
     },
   },
 });
+
+let animItems = document.querySelectorAll('.animation-items');
+if (animItems.length > 0) {
+  window.addEventListener ('scroll', animOnScroll);
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animstart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animstart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window. innerHeight - window.innerHeight / animstart;
+      }
+
+      if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('_active');
+      }
+      else {
+        if(!animItem.classList.contains('_anim-no-hide')) {
+          animItem.classList.remove('_active')
+        }
+      }
+    }
+  }
+
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollleft = window-pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollleft }
+  }
+  animOnScroll();
+}
